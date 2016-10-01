69abbc9b375654eae83fd3a200085764e861bfd2
Marko Nikic <darkblue.azurite@gmail.com>
2016-10-01 18:55:36 +0200

    added hash-url.js

--- app/db_operations/add-url.js
+++ app/db_operations/add-url.js
@@ -1,14 +1,20 @@
 var isValid = require("../validate-url.js");
+var hash = require("../hash-url.js");
 
 module.exports = function(mongoClient) {
   return function(req, res) {
     var url = req.params.url;
 
     if(isValid(url)) {
-      res.send("Valid url: " + url);
+      // following:
+      // 1. hash the url string
+      // 2. lookup in the db if the hash already exists if yes send that entry else create a new entry
+      // 3. send the entry with both urls (short and long)
     }
     else {
-      res.send("Invalid url!");
+      res.json({
+        error: "Invalid URL"
+      });
     }
   };
 };
---
+++ app/hash-url.js
@@ -0,0 +1,15 @@
+module.exports = function(url) {
+  if(typeof url !== "string") {
+    return false;
+  }
+  var hash = 0, char = 0;
+  if(url.length === 0) {
+    return hash;
+  }
+  for(var i = 0; i < url.length; i++) {
+    char = url.charCodeAt(i);
+    hash =((hash << 5) - hash) + char;
+    hash = hash & hash; //convert to 32-bit integer
+  }
+  return hash.toString();
+};
