f14431f9bef803cd704d1174abf92298de8d2647
Marko Nikic <darkblue.azurite@gmail.com>
2016-10-01 16:02:00 +0200

    finshed inital server

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+var logger = require("connect-logger");
+var errorHandler = require("errorhandler");
+var _static = require("serve-static");
+
+var path = require("path");
 var http = require("http");
+
 var express = require("express");
 var mongodb = require("mongodb");
 
 var app = express();
 
+if(app.get("env") === "development") {
+  app.use(logger());
+  app.use(errorHandler());
+}
+
+app.use(_static(path.join(__dirname + "/public")));
+
 var server = http.createServer(app).listen(process.env.PORT || 8080, (err) => {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log("Server listenin on port: " + server.address().port);
+  if(app.get("env") === "development") {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("Server listenin on port: " + server.address().port);
+    }
   }
 });
