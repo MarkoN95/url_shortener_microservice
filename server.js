f00cf89ebd1a19e08a5fb6e3edbf73f98ecf532c
Marko Nikic <darkblue.azurite@gmail.com>
2016-10-01 15:26:42 +0200

    finished setup

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 var http = require("http");
 var express = require("express");
 var mongodb = require("mongodb");
+
+var app = express();
+
+var server = http.createServer(app).listen(process.env.PORT || 8080, (err) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("Server listenin on port: " + server.address().port);
+  }
+});
